b6de5c12a13780b83a9b35b978d73d9a5cec70be
amazingchow <jianzhou42@163.com>
2020-07-15 15:41:50 +0800

    [Books] add GET method

--- server.py
+++ server.py
@@ -13,5 +13,31 @@ def ping_pong():
     return jsonify('pong')
 
 
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': BOOKS
+    })
+
+
 if __name__ == "__main__":
     app.run(debug=True)
